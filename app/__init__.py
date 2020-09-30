13e084b68928f37839856e37bc09b79140b944be
caiseyann <caiseyann4@gmail.com>
2020-09-30 15:04:48 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 
-bootstrap = Bootstrap()
-
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 def create_app(config_name):
 
     #Initialize app
@@ -14,14 +17,15 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # Initializing Flask Extensions
-    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
+    from app.auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
-
-
+    app.add_url_rule('/', endpoint='main.index')
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')  
 
 
     return app
-
