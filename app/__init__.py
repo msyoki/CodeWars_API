78ffd22a432044e7c60a519cedfa127e53b4d888
msyoki <msyokimutua@gmail.com>
2020-10-01 12:46:36 +0300

    Update image instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -24,6 +26,9 @@ def create_app(config_name):
     bootstrap = Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     # Registering the blueprint
     from .main import main as main_blueprint
